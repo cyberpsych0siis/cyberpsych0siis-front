c45701c0c4e7d922ad5be7896c773d9027757978
Philipp Gaßner <activator112233@gmail.com>
2022-06-17 17:50:19 +0200

    renewed deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 docker stop frontend
 docker rm frontend
 docker build -t cyberpsych0siis-front .
-docker run -d --name frontend --restart=always cyberpsych0siis-front
+docker run -d --name frontend --restart=always -v $(pwd):/usr/share/nginx/html cyberpsych0siis-front
